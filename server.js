d45d7aa8bd6d50498fd4f7f63356a1db60670c1b
joaofp <jvfpdaemoon1@gmail.com>
2022-09-02 13:46:39 -0300

    post/participants: 95%;get/participants:100%;post/messages:70%

--- server.js
+++ server.js
@@ -2,27 +2,92 @@ import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from "dotenv";
+import Joi from 'joi';
+import dayjs from 'dayjs';
+import customParseFormat from "dayjs/plugin/customParseFormat.js";
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
+dayjs.extend(customParseFormat);
+dayjs.locale('de');
 
-console.log(process.env.MONGO_URI);
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
-
 
 mongoClient.connect().then(() => {
     db = mongoClient.db("bate_papo_uol");
 })
 
-app.get('/', (req, res)=> {
-    res.send('testando');
+app.post('/participants', async (req, res) => {
+    let { name, lastStatus } = req.body;
+    const time = dayjs(Date.now(), "HH:MM:SS", 'de');
+    const statusMessage = {
+        from: name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: time
+    };
+    const schema = Joi.object({
+        name: Joi.string().min(1).required(),
+    });
+    try {
+        const value = await schema.validateAsync({ name });
+        console.log('deu bom');
+    } catch (error) {
+        res.status(422).send("Campo obrigatório!");
+    }
+    lastStatus = Date.now();
+    const participantsResult = await db.collection('participants').insertOne({name, lastStatus}).then(() => {
+        res.send('Ok');
+    });
+    const messageResult = await db.collection('messages').insertOne(statusMessage).then(() => {
+        console.log('Status enviado.')
+    })
+    res.status(201).send;
 });
 
+app.get('/participants', (req, res) => {
+    db.collection('participants').find().toArray().then(data => {
+        res.send(data);
+        console.log(data);
+    })
+})
+
+app.post('/messages', async (req, res) => {
+    const { to, text, type} = req.body;
+    const { from } = req.headers;
+
+    const schema = Joi.object({
+        to: Joi.string().min(1).required(),
+        text: Joi.string().min(1).required(),
+        type: Joi.string().valid('message', 'private_message').required()
+    })
+    try {
+        const value = await schema.validateAsync({to, text, type});
+        console.log('Funcionou');
+    } catch (error) {
+        res.status(422).send('deu ruim');
+    }
+
+    const time = dayjs(Date.now(), "HH:MM:SS", 'de');
+
+    const result = await db.collection('messages').insertOne({to, type, from, time, text}).then(() => {
+        res.send('Ok');
+    });
+    res.status(201).send;
+})
 
 
-console.log('teste')
 
-app.listen(5000);
+
+
+
+
+
+
+
+app.listen(5000, () => { console.log("Ouvindo")});
